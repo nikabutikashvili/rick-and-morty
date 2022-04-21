88b938c85add6ba9bb8d9952e59fff2df1b851b1
Nika Butikashvili <butikashvilinika@gmail.com>
2022-04-21 15:53:30 +0200

    add login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
+import Logo from "../assets/images/logo.png";
+import styles from "../styles/Login.module.css";
+
+export interface User {
+  username: string;
+  password: string;
+}
+
 const Login: React.FC = () => {
+  const [user, setUser] = useState<User>({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className={styles.wrapper}>
+      <img className={styles.logo} src={Logo} alt="logo" />
+      <div>
+        <h1>Please login in order to see characters</h1>
+        <form className={styles.form}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            value={user.username}
+            onChange={handleChange}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <button className="button-1" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+      <button className="button-1" type="button">
+        Register
+      </button>
     </div>
   );
 };
