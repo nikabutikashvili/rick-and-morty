d574bf49881e2c18fd86563389749dbfb689995e
Nika Butikashvili <butikashvilinika@gmail.com>
2022-04-22 13:24:52 +0200

    add reusable axios instrance & define API-URL .env

---
+++ client/src/configs/api/index.ts
@@ -0,0 +1,39 @@
+import Axios, { AxiosError, AxiosRequestConfig } from "axios";
+const API_URL = process.env.REACT_APP_API_URL;
+
+const api = Axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+api.interceptors.request.use(
+  async (config: AxiosRequestConfig) => {
+    const token = false;
+
+    if (token) {
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer {token}`,
+      };
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => {
+    if (response?.data?.message) {
+    }
+    return response;
+  },
+  async (error: AxiosError) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+export { api };
