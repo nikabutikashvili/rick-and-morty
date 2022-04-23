b019efcb131d5ed716a1dba4d1e506d78a2a3f5a
Nika Butikashvili <butikashvilinika@gmail.com>
2022-04-23 19:26:18 +0200

    add credentials on cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,7 +4,12 @@ import cookieParser from "cookie-parser";
 import "dotenv/config";
 
 const app: Express = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
